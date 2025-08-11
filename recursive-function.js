bf850963e3241041f40bcf1d6befb98e193fa1f2
hursha <hurshaparveen@gmail.com>
2025-08-10 20:25:34 -0500

    Revers String

--- recursive-function.js
+++ recursive-function.js
@@ -7,12 +7,12 @@ function reverseString(str) {
     // Recursive Case: Last character + reversed remaining string
     return str.charAt(str.length - 1) + reverseString(str.slice(0, -1));
 }
-console.log(reverseString("hello"));     // Outputs: "olleh"
-console.log(reverseString("recursion")); // Outputs: "noisrucer"
-console.log(reverseString("a"));         // Outputs: "a"
-console.log(reverseString(""));          // Outputs: ""
-console.log(reverseString("my pumpkin"));     // Outputs: "nikpmup ym"
-console.log(reverseString("launchcode")); // Outputs: "edochcnual"
-console.log(reverseString("welcome"));         // Outputs: "emoclew"
-console.log(reverseString("happy"));          // Outputs: "yppah"    
+console.log(reverseString("hello"));       // Outputs: "olleh"
+console.log(reverseString("recursion"));   // Outputs: "noisrucer"
+console.log(reverseString("a"));           // Outputs: "a"
+console.log(reverseString(""));            // Outputs: ""
+console.log(reverseString("my pumpkin"));  // Outputs: "nikpmup ym"
+console.log(reverseString("launchcode"));  // Outputs: "edochcnual"
+console.log(reverseString("welcome"));     // Outputs: "emoclew"
+console.log(reverseString("happy"));       // Outputs: "yppah"    
 
